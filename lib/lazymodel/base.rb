b5f4951e037752a379dbddcbf12c1e9929feb560
andrea longhi <andrea@spaghetticode.it>
2012-08-08 21:47:15 +0200

    use activemodel lint to comply to activemodel api

--- lib/lazymodel/base.rb
+++ lib/lazymodel/base.rb
@@ -1,25 +1,57 @@
 module Lazymodel
   class Base
+    extend ActiveModel::Naming
+    include ActiveModel::Conversion
+    include ActiveModel::Validations
     include ActiveModel::AttributeMethods
 
-    def self.has_attributes(*args)
-      opts = args.extract_options!
-      attr_accessor *args
-      define_prefixed_or_affixed(:prefix, opts[:prefix])
-      define_prefixed_or_affixed(:suffix, opts[:suffix])
-      define_attribute_methods args
-    end
+    class << self
+      def has_attributes(*args)
+        opts = args.extract_options!
+        # define attributes and prefixed/suffixed methods
+        attr_accessor *args
+        define_prefix_or_affix_method :prefix, opts[:prefix]
+        define_prefix_or_affix_method :suffix, opts[:suffix]
+        define_attribute_methods args
+        # _attributes stores at class level the
+        # the attribute names
+        class_attribute :_attributes
+        self._attributes = args
+      end
 
-    private
+      private
 
-    def self.define_prefixed_or_affixed(type, values)
-      Array.wrap(values).each do |value|
-        send "attribute_method_#{type}", value
-        method = type == :prefix ? "#{value}attribute" : "attribute#{value}"
-        define_method method do |*args|
-          raise "You must define the method #{method} on your own"
+      def define_prefix_or_affix_method(type, values)
+        Array.wrap(values).each do |value|
+          send "attribute_method_#{type}", value
+          method = type == :prefix ? "#{value}attribute" : "attribute#{value}"
+          define_method method do |*args|
+            raise "You must define the method #{method} on your own"
+          end
         end
       end
+    end
+
+    # don't override this method without calling super!
+    def initialize(attributes={})
+      attributes.each do |attribute, value|
+        send("#{attribute}=", value) if value.present?
+      end
+    end
+
+    def attributes
+      _attributes.inject(HashWithIndifferentAccess.new) do |hash, attribute|
+        hash.update attribute => send(attribute)
+      end
+    end
+
+    def to_model
+      self
+    end
+
+    # override at need
+    def persisted?
+      false
     end
   end
 end
--- test/sample_model_test.rb
+++ test/sample_model_test.rb
@@ -2,18 +2,19 @@ require 'test_helper'
 require 'fixtures/sample_model'
 
 class SampleModelTest < ActiveSupport::TestCase
+  include ActiveModel::Lint::Tests
+
   def setup
     @model = SampleModel.new
+    set_model_attributes
   end
 
   test 'has name and surname attributes' do
-    set_model_attributes
     assert_equal 'john', @model.name
     assert_equal 'doe', @model.surname
   end
 
   test 'has clear prefix for attributes' do
-    set_model_attributes
     @model.clear_name
     @model.clear_surname
     assert_nil @model.name
@@ -21,9 +22,23 @@ class SampleModelTest < ActiveSupport::TestCase
   end
 
   test 'has ? suffix attributes' do
-    @model.name = 'john'
+    @model.clear_surname
     assert @model.name?
     assert !@model.surname?
+  end
+
+  test 'accepts attributes on initialization' do
+    model = SampleModel.new(:name => 'john')
+    assert_equal 'john', model.name
+  end
+
+  test 'has expected attributes' do
+    attributes = {
+      'name'    => 'john',
+      'surname' => 'doe'
+    }
+    assert_empty attributes.keys - @model.attributes.keys
+    assert_empty attributes.values - @model.attributes.values
   end
 
   private
