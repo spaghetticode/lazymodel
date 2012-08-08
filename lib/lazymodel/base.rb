ae8a8d6209b6703b0bc3370f740824e8c179da5b
andrea longhi <andrea@spaghetticode.it>
2012-08-08 19:43:13 +0200

    refactoring

--- lib/lazymodel/base.rb
+++ lib/lazymodel/base.rb
@@ -5,19 +5,21 @@ module Lazymodel
     def self.has_attributes(*args)
       opts = args.extract_options!
       attr_accessor *args
-      Array.wrap(opts[:prefix]).each do |prefix|
-        attribute_method_prefix prefix
-        define_method "#{prefix}attribute" do |*args|
-          raise "You must define the method #{prefix}attribute on your own"
-        end
-      end
-      Array.wrap(opts[:suffix]).each do |suffix|
-        attribute_method_suffix suffix
-        define_method "attribute#{suffix}" do |*args|
-          raise "You must define the method attribute#{suffix} on your own"
-        end
-      end
+      define_prefixed_or_affixed(:prefix, opts[:prefix])
+      define_prefixed_or_affixed(:suffix, opts[:suffix])
       define_attribute_methods args
+    end
+
+    private
+
+    def self.define_prefixed_or_affixed(type, values)
+      Array.wrap(values).each do |value|
+        send "attribute_method_#{type}", value
+        method = type == :prefix ? "#{value}attribute" : "attribute#{value}"
+        define_method method do |*args|
+          raise "You must define the method #{method} on your own"
+        end
+      end
     end
   end
 end
