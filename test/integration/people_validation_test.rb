e02f135cd14d1d8259f4aebd153eb34797e22012
andrea longhi <andrea@spaghetticode.it>
2012-08-09 11:36:04 +0200

    improve readability

--- test/integration/people_validation_test.rb
+++ test/integration/people_validation_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class PeopleValidationClass < ActiveSupport::TestCase
   test 'renders person form' do
     visit '/people/new'
-    assert_match /Validate Person/, page.body
+    assert_match 'Validate Person', page.body
   end
 
   test 'successfully validates valid person' do
@@ -11,13 +11,13 @@ class PeopleValidationClass < ActiveSupport::TestCase
     fill_in 'Name', :with => 'john'
     fill_in 'Surname', :with => 'doe'
     click_button 'Validate'
-    assert_match /Person is valid/, page.body
+    assert_match 'Person is valid', page.body
   end
 
   test 'does not validate invalid person' do
     visit '/people/new'
     fill_in 'Name', :with => 'john'
     click_button 'Validate'
-    assert_match /Person is not valid/, page.body
+    assert_match 'Person is not valid', page.body
   end
 end
